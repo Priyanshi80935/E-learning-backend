a7b604b9b1f42c424daf360ed2195ee268118e36
Priyanshi80935 <165469120+Priyanshi80935@users.noreply.github.com>
2024-10-23 23:57:41 +0530

    backend new changes

--- e-learning-backend/src/main/java/com/tech/controller/CourseController.java
+++ e-learning-backend/src/main/java/com/tech/controller/CourseController.java
@@ -1,15 +1,32 @@
 package com.tech.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // @WebServlet("/api/course") like
+import com.tech.dto.Course;
+
+@RestController // @WebServlet("/api/course") like 
 @RequestMapping(value ="/api/course") //Endpoint - Connect from UI
 public class CourseController {
 	
 	@GetMapping
-	public String index() {
-		return "BOOT Started";
+	public List<Course> index() {
+		return Arrays.asList( new Course(1, "Angular", 
+			        "Angular is a platform and framework for building single-page client applications using HTML and TypeScript.", 
+			        100, 4, "images/angular.png", "2 months", 1000, "12", 103),
+			        
+			    new Course(2, "React", 
+			        "React is a declarative, efficient, and flexible JavaScript library for building user interfaces.", 
+			        200, 3, "images/react.png", "3 months", 2000, "15", 100),
+			        
+			    new Course(3, "Vue", 
+			        "Vue.js is a progressive framework for building user interfaces.", 
+			        150, 5, "images/vue.png", "2.5 months", 1500, "14", 100)
+			);
+
 	}
 }
---
+++ e-learning-backend/src/main/java/com/tech/dto/Course.java
@@ -0,0 +1,23 @@
+package com.tech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private double rating;
+    private String img;
+    private String duration;
+    private int noOfStudents;
+    private String lecture;
+    private int ratingCount;
+}
