a6fb69e8a1ebee5578cf48ac40b547eda3ec03b4
Priyanshi80935 <165469120+Priyanshi80935@users.noreply.github.com>
2024-10-27 10:25:28 +0530

    coursecontroller added

--- e-learning-backend/src/main/java/com/tech/controller/CourseController.java
+++ e-learning-backend/src/main/java/com/tech/controller/CourseController.java
@@ -1,9 +1,12 @@
 package com.tech.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +16,16 @@ import com.tech.dto.Course;
 @RequestMapping(value ="/api/course") //Endpoint - Connect from UI
 public class CourseController {
 	
+	List<Course> courses = new ArrayList<>();
+	
 	@GetMapping
 	public List<Course> index() {
-		return Arrays.asList( new Course(1, "Angular", 
-			        "Angular is a platform and framework for building single-page client applications using HTML and TypeScript.", 
-			        100, 4, "images/angular.png", "2 months", 1000, "12", 103),
-			        
-			    new Course(2, "React", 
-			        "React is a declarative, efficient, and flexible JavaScript library for building user interfaces.", 
-			        200, 3, "images/react.png", "3 months", 2000, "15", 100),
-			        
-			    new Course(3, "Vue", 
-			        "Vue.js is a progressive framework for building user interfaces.", 
-			        150, 5, "images/vue.png", "2.5 months", 1500, "14", 100)
-			);
+		return courses;
 
+	}
+	
+	@PostMapping
+	public void save(@RequestBody Course course) {
+		courses.add(course);
 	}
 }
