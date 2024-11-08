3a8fbfe6fcea1228216d8c9c4db28f27a29580ed
Satya Prakash Biswal <satyaprakashkolkata@gmail.com>
2024-11-09 00:06:13 +0530

    updated

--- e-learning-backend/src/main/java/com/tech/entity/ApplicationUser.java
+++ e-learning-backend/src/main/java/com/tech/entity/ApplicationUser.java
@@ -1,5 +1,6 @@
 package com.tech.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,8 @@ public class ApplicationUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name="username", unique = true)
     private String username;
     private String password;
     private String role;
